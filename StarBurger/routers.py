da8d3f2fa9d258d5987e3e928e9b9fac4f960fac
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2021-03-23 11:23:21 +0700

    Remove redundant urls

--- StarBurger/routers.py
+++ StarBurger/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 
-from users.views import UsersViewSet, CustomAuthToken
+from users.views import UsersViewSet
 
 router = routers.DefaultRouter()
 router.register('reg', UsersViewSet, basename='reg')
