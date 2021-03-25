ee02eaec241e33f859b2b8e4e06a0540ad36e3d5
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2021-03-25 23:20:46 +0700

    Add factories

---
+++ foodcartapp/factories.py
@@ -0,0 +1,52 @@
+import factory.fuzzy
+
+from foodcartapp import models
+
+
+class ProductCategoryFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker('sentence', nb_words=2)
+
+    class Meta:
+        model = models.ProductCategory
+
+
+class RestaurantFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker('sentence', nb_words=2)
+    address = factory.Faker('address', locale='ru_RU')
+    contact_phone = factory.Faker('phone_number', locale='ru_RU')
+
+    class Meta:
+        model = models.Restaurant
+
+
+class OrderFactory(factory.django.DjangoModelFactory):
+    firstname = factory.Faker('first_name')
+    lastname = factory.Faker('last_name')
+    phonenumber = factory.Faker('phone_number', locale='ru_RU')
+    address = factory.Faker('address', locale='ru_RU')
+
+    class Meta:
+        model = models.Order
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker('sentence', nb_words=3)
+    category = ProductCategoryFactory()
+    price = factory.fuzzy.FuzzyDecimal(150.00, 400.00)
+    image = factory.django.ImageField(
+        color=factory.fuzzy.FuzzyChoice(['magenta', 'blue', 'green']),
+    )
+    special_status = factory.fuzzy.FuzzyChoice([True, False])
+    description = factory.Faker('text')
+
+    class Meta:
+        model = models.Product
+
+
+class OrderItemFactory(factory.django.DjangoModelFactory):
+    product = factory.Iterator(models.Product.objects.all())
+    quantity = factory.fuzzy.FuzzyInteger(1, 10)
+    order = factory.Iterator(models.Order.objects.all())
+
+    class Meta:
+        model = models.OrderItem
