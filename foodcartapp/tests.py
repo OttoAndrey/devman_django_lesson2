a65e6784b5ae0a4b44fbf70bd8e4d3952d5de3f5
ray <ottotruepro@gmail.com>
2021-03-27 19:47:39 +0700

    Add tests

--- foodcartapp/tests.py
+++ foodcartapp/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from foodcartapp.factories import ProductFactory, ProductCategoryFactory
+from foodcartapp.models import Order, OrderItem
 
 
 class RegisterOrderTests(APITestCase):
@@ -12,16 +13,36 @@ class RegisterOrderTests(APITestCase):
         ProductFactory.create_batch(2)
 
     def test_register_order(self):
-        self.data = {
-            "products": [
-                {"product": 1, "quantity": 1},
-                {"product": 2, "quantity": 2}
+        """Expected correct order saving and answer about user."""
+        orders_count_before = Order.objects.count()
+        order_items_count_before = OrderItem.objects.count()
+        data = {
+            'products': [
+                {'product': 1, 'quantity': 1},
+                {'product': 2, 'quantity': 2}
             ],
-            "firstname": "Ivan",
-            "lastname": "Ivanov",
-            "phonenumber": "79138887766",
-            "address": "Москва, Тверская 1"
+            'firstname': 'Ivan',
+            'lastname': 'Ivanov',
+            'phonenumber': '79138887766',
+            'address': 'Москва, Тверская 1'
         }
-        url = reverse("foodcartapp:register-order")
-        response = self.client.post(url, self.data, format="json")
+        url = reverse('foodcartapp:register-order')
+        response = self.client.post(url, data, format='json')
+        last_order = Order.objects.last()
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Order.objects.count(), orders_count_before + 1)
+        self.assertEqual(
+            OrderItem.objects.count(),
+            order_items_count_before + 2,
+        )
+        self.assertEqual(last_order.firstname, data['firstname'])
+        self.assertEqual(last_order.phonenumber, data['phonenumber'])
+        self.assertEqual(
+            last_order.items.first().id,
+            data['products'][0]['product'],
+        )
+        self.assertEqual(
+            last_order.items.first().quantity,
+            data['products'][0]['quantity'],
+        )
--- restaurateur/tests.py
+++ restaurateur/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from foodcartapp.factories import (
+    OrderWithProductsFactory,
+    ProductCategoryFactory,
+    ProductFactory,
+    RestaurantWithAllAvailabilityProductsFactory,
+    RestaurantWithoutAllAvailabilityProductsFactory,
+)
+from foodcartapp.models import Order, Restaurant
+from restaurateur.factories import SuperUserFactory
+from restaurateur.views import (
+    get_available_restaurants,
+    get_distance_between_restaurants_and_order,
+)
+
+
+class ServicesTests(TestCase):
+
+    def setUp(self):
+        SuperUserFactory()
+        ProductCategoryFactory.create_batch(3)
+        ProductFactory.create_batch(2)
+        OrderWithProductsFactory()
+        RestaurantWithoutAllAvailabilityProductsFactory()
+        self.possible_restaurant = (
+            RestaurantWithAllAvailabilityProductsFactory()
+        )
+
+    def test_get_available_restaurants(self):
+        order = Order.objects.last()
+        available_restaurants = get_available_restaurants(order)
+
+        self.assertEqual(Restaurant.objects.count(), 2)
+        self.assertEqual(len(available_restaurants), 1)
+        self.assertEqual(
+            available_restaurants[0].name,
+            self.possible_restaurant.name,
+        )
+
+    @patch('restaurateur.views.get_coordinates')
+    def test_get_distance_between_restaurants_and_order(
+            self,
+            mock_get_coordinates,
+    ):
+        get_coordinates = mock_get_coordinates()
+        get_coordinates.return_value = ('55.7522', '37.6156')
+        order = Order.objects.last()
+        available_restaurants = get_available_restaurants(order)
+        restaurants_with_distance = get_distance_between_restaurants_and_order(
+            order,
+            available_restaurants,
+        )
+
+        self.assertEqual(type(restaurants_with_distance), dict)
+        self.assertEqual(
+            len(restaurants_with_distance),
+            len(available_restaurants),
+        )
+        self.assertIn(available_restaurants[0], restaurants_with_distance)
+
+    @patch('restaurateur.views.get_coordinates')
+    def test_get_coordinates(self, mock_get_coordinates):
+        get_coordinates = mock_get_coordinates()
+        get_coordinates.return_value = ('55.7522', '37.6156')
+        coordinates = get_coordinates(self.possible_restaurant)
+
+        self.assertEqual(len(coordinates), 2)
+        self.assertEqual(type(coordinates[0]), str)
+        self.assertEqual(type(coordinates[1]), str)
+
+    @patch('restaurateur.views.get_coordinates', return_value=(
+        '55.7522',
+        '37.6156',
+    ))
+    def test_view_orders(self, mock_get_coordinates):
+        self.client.force_login(User.objects.first())
+        orders_url = reverse('restaurateur:view_orders')
+        response = self.client.get(orders_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            len(response.context['order_items']),
+            Order.objects.count(),
+        )
+        for order_item in response.context['order_items']:
+            self.assertTrue(hasattr(
+                order_item,
+                'available_restaurants_with_distance',
+            ))
