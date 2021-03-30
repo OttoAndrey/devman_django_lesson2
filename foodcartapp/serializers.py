97639698abe4dc8102d4402bf20a58a4418df8a7
ray <ottotruepro@gmail.com>
2021-03-30 18:15:05 +0700

    Add data about items to order serializer

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Product
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name']
+
+
+class OrderItemDataSerializer(ModelSerializer):
+    product_data = ProductSerializer(source='product')
+
+    class Meta:
+        model = OrderItem
+        fields = ['product_data', 'price', 'quantity', ]
 
 
 class OrderItemSerializer(ModelSerializer):
@@ -11,8 +25,25 @@ class OrderItemSerializer(ModelSerializer):
 
 
 class OrderSerializer(ModelSerializer):
-    products = OrderItemSerializer(many=True, allow_empty=False, write_only=True)
+    products = OrderItemSerializer(
+        many=True,
+        allow_empty=False,
+        write_only=True,
+    )
+    items_data = OrderItemDataSerializer(
+        many=True,
+        source='items',
+        read_only=True,
+    )
 
     class Meta:
         model = Order
-        fields = ['id', 'firstname', 'lastname', 'phonenumber', 'address', 'products', ]
+        fields = [
+            'id',
+            'firstname',
+            'lastname',
+            'phonenumber',
+            'address',
+            'products',
+            'items_data',
+        ]
