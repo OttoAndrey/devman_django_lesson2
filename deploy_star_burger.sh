b880f50d17b6333c548b2c0c5c383e7d02447536
Ray <ottotruepro@gmail.com>
2023-04-23 13:24:57 +0600

    Fix deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,15 +3,16 @@ set -e
 echo "Pull project from remote repo"
 git pull
 echo "Update backend dependencies"
+source .venv/bin/activate
 pip3 install -r requirements.txt
 echo "Update frontend dependencies"
 npm install
 echo "Update frontend"
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 echo "Update Django static"
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 echo "Migrations for Django"
 python3 manage.py migrate
 echo "Systemd reload"
-systemctl reload starburger.service
+systemctl start starburger.service
 echo "Deploy ended successfully"
